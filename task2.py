5198e43fc9bd52cfef6b0a6ddd0c084f97a9ec53
ShanshanJia <79418356@qq.com>
2017-12-30 03:50:01 +0000

    Update Task2: Pull Page 1-10 of the forum website, and dispose the old items posted more than one week ago

--- task2.py
+++ task2.py
@@ -6,19 +6,22 @@ import requests
 from bs4 import BeautifulSoup
 
 url = 'http://bbs.skykiwi.com/forum.php'
-payload = {'mod': 'forumdisplay', 'fid': '17', 'page': '1'}
 header = {'user-agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.108 Safari/537.36'}
-r = requests.get(url, params=payload, headers=header)
-
-soup = BeautifulSoup(r.text, "lxml")
-tbodyList = soup.select("tbody[id*=normalthread_]")
-
 baseTable = []
-for tbody in tbodyList:
-    posts = tbody.select('a[class="xst"]')
-    postTitle = posts[0].string
-    postLink = 'http://bbs.skykiwi.com/' + posts[0]['href']
-    postTime = tbody.select("span[title*=20]")
-    postDate = postTime[0]['title'].split()
-    baseTable.append([postTitle, postLink, postDate[0]])
-print(baseTable)
+
+for i in range(1, 11):
+    payload = {'mod': 'forumdisplay', 'fid': '17', 'page': str(i)}
+    pages = requests.get(url, params=payload, headers=header)
+    soup = BeautifulSoup(pages.text, "lxml")
+    tbodyList = soup.select("tbody[id*=normalthread_]")
+    for tbody in tbodyList:
+        if tbody.select("span[title*=20]") != []:
+            postTime = tbody.select("span[title*=20]")
+            postDate = postTime[0]['title'].split()
+            posts = tbody.select('a[class="xst"]')
+            postTitle = posts[0].string
+            postLink = 'http://bbs.skykiwi.com/' + posts[0]['href']
+            baseTable.append([postTitle, postLink, postDate[0]])
+        else:
+            tbody.decompose()
+print(len(baseTable))
